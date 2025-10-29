67f167411e4df6a40ed4c037e5d3d9369d9d4556
Makar Abarin <mabarin@kept.ru>
2025-10-29 18:12:34 +0300

    Базы данных, продвинутые запросы: Добавление таблиц с "удобствами" и M2M связи

---
+++ src/migrations/versions/2025_10_29_1807-a196d22c785d_add_facilities.py
@@ -0,0 +1,49 @@
+"""add facilities
+
+Revision ID: a196d22c785d
+Revises: f6b26083da90
+Create Date: 2025-10-29 18:07:54.744743
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision: str = "a196d22c785d"
+down_revision: Union[str, Sequence[str], None] = "f6b26083da90"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table(
+        "facilities",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("title", sa.String(length=100), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_table(
+        "rooms_facilities",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("room_id", sa.Integer(), nullable=False),
+        sa.Column("facility_id", sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["facility_id"],
+            ["facilities.id"],
+        ),
+        sa.ForeignKeyConstraint(
+            ["room_id"],
+            ["rooms.id"],
+        ),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_table("rooms_facilities")
+    op.drop_table("facilities")
