fdaad0155edbbc4d7bd5f6223af3f6156faf7920
Makar Abarin <mabarin@kept.ru>
2025-12-09 17:55:13 +0300

    Redis: разработка класса и подключение к базе

--- src/config.py
+++ src/config.py
@@ -7,6 +7,9 @@ class Settings(BaseSettings):
     DB_PASS: str
     DB_NAME: str
 
+    REDIS_HOST: str
+    REDIS_PORT: int
+
     @property
     def db_url(self): # адрес для подключения к БД - DSN - формат строки подключения
         return f'postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
