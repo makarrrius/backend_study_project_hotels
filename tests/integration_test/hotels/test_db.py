4d59ba50e55d5deaf71f60853ecad55483a19a6c
Makar Abarin <mabarin@kept.ru>
2026-01-07 15:21:13 +0300

    Тесты: тест на добавление отелей

---
+++ tests/integration_test/hotels/test_db.py
@@ -0,0 +1,10 @@
+from src.schemas.hotels import HotelAdd
+from src.utils.db_manager import DBManager
+from src.database import async_session_maker
+
+
+async def test_add_hotel():
+    hotel_data = HotelAdd(title="Test Hotel", description="Test Description")
+    async with DBManager(session_factory=async_session_maker) as db:
+        new_hotel_data = await db.bookings.add(hotel_data)
+        print(f'{new_hotel_data=}')
